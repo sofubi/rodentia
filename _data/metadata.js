172b857b5e5891df3fc791201a3c9a67ef8222a0
Thomas Lawton <thomas.lawton@ratmail.cc>
2024-11-05 21:40:33 -0500

    Update sitename

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
 	title: "Thomas Lawton | Rodentia",
-	url: "https://rodentia.xyz",
+	url: "https://rodentia.cc",
 	language: "en",
 	description: "Thoughts posted on occasion.",
 	author: {
 		name: "Thomas Lawton",
 		email: "thomas.lawton@ratmail.cc",
-		url: "https://rodentia.xyz/about/",
+		url: "https://rodentia.cc/about/",
 	},
 };
